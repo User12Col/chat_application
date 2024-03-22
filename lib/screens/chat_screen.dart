95254cc0e0f57b5591da41137e6d013cbfdf81ad
loc <votrantanloc140@gmail.com>
2024-03-22 09:25:05 +0700

    fix chat content

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,17 +1,24 @@
-import 'package:chat_app/models/message_data.dart';
+import 'dart:async';
+
+import 'package:chat_app/app.dart';
+import 'package:chat_app/helpers.dart';
+import 'package:chat_app/widgets/display_error_message.dart';
+import 'package:collection/collection.dart';
 import 'package:chat_app/theme.dart';
 import 'package:chat_app/widgets/avatar.dart';
 import 'package:chat_app/widgets/glowing_action_button.dart';
 import 'package:chat_app/widgets/icon_button.dart';
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
 class ChatScreen extends StatelessWidget {
   final Channel channel;
   const ChatScreen({Key? key, required this.channel}) : super(key: key);
 
-  static Route routeWithChannel(Channel channel) =>
-      MaterialPageRoute(builder: (context) => StreamChannel(child: ChatScreen(channel: channel), channel: channel));
+  static Route routeWithChannel(Channel channel) => MaterialPageRoute(
+      builder: (context) =>
+          StreamChannel(child: ChatScreen(channel: channel), channel: channel));
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +38,7 @@ class ChatScreen extends StatelessWidget {
             },
           ),
         ),
-        // title: _AppBarTitle(
-        //   messageData: messageData,
-        // ),
+        title: _AppBarTitle(),
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
@@ -51,7 +56,18 @@ class ChatScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Expanded(child: _DemoMessageList()),
+          Expanded(
+            child: MessageListCore(
+              loadingBuilder: (context) {
+                return const Center(child: CircularProgressIndicator());
+              },
+              emptyBuilder: (context) => const SizedBox.shrink(),
+              errorBuilder: (context, error) =>
+                  DisplayErrorMessage(error: error),
+              messageListBuilder: (context, messages) =>
+                  _MessageList(messages: messages),
+            ),
+          ),
           _ActionBar(),
         ],
       ),
@@ -59,15 +75,124 @@ class ChatScreen extends StatelessWidget {
   }
 }
 
-class _AppBarTitle extends StatelessWidget {
-  final MessageData messageData;
-  const _AppBarTitle({Key? key, required this.messageData}) : super(key: key);
+class _MessageList extends StatelessWidget {
+  const _MessageList({
+    Key? key,
+    required this.messages,
+  }) : super(key: key);
+
+  final List<Message> messages;
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.separated(
+        itemCount: messages.length + 1,
+        reverse: true,
+        separatorBuilder: (context, index) {
+          if (index == messages.length - 1) {
+            return _DateLable(dateTime: messages[index].createdAt);
+          }
+          if (messages.length == 1) {
+            return const SizedBox.shrink();
+          } else if (index >= messages.length - 1) {
+            return const SizedBox.shrink();
+          } else if (index <= messages.length) {
+            final message = messages[index];
+            final nextMessage = messages[index + 1];
+            // if (Jiffy.parse(message.createdAt.toLocal().toString()).isSame(
+            //     Jiffy.parse(nextMessage.createdAt.toLocal().toString()))) {
+            //   return _DateLable(
+            //     dateTime: message.createdAt,
+            //   );
+            // } else {
+            //   return const SizedBox.shrink();
+            // }
+            return _DateLable(
+                     dateTime: message.createdAt,
+                   );
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
+        itemBuilder: (context, index) {
+          if (index < messages.length) {
+            final message = messages[index];
+            if (message.user?.id == context.currentUser?.id) {
+              return _MessageOwnTile(message: message);
+            } else {
+              return _MessageTile(message: message);
+            }
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class _AppBarTitle extends StatelessWidget {
+  const _AppBarTitle({Key? key}) : super(key: key);
+
+  Widget _buildConnectedTitleState(
+    BuildContext context,
+    List<Member>? members,
+  ) {
+    Widget? alternativeWidget;
+    final channel = StreamChannel.of(context).channel;
+    final memberCount = channel.memberCount;
+    if (memberCount != null && memberCount > 2) {
+      var text = 'Members: $memberCount';
+      final watcherCount = channel.state?.watcherCount ?? 0;
+      if (watcherCount > 0) {
+        text = 'watchers $watcherCount';
+      }
+      alternativeWidget = Text(
+        text,
+      );
+    } else {
+      final userId = StreamChatCore.of(context).currentUser?.id;
+      final otherMember = members?.firstWhereOrNull(
+        (element) => element.userId != userId,
+      );
+
+      if (otherMember != null) {
+        if (otherMember.user?.online == true) {
+          alternativeWidget = const Text(
+            'Online',
+            style: TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          );
+        } else {
+          alternativeWidget = Text(
+            'Last online: '
+            '${Jiffy.parse(otherMember.user!.lastActive.toString()).fromNow()}',
+            style: const TextStyle(
+              fontSize: 8,
+              fontWeight: FontWeight.bold,
+              color: Colors.red,
+            ),
+          );
+        }
+      }
+    }
+    return TypingIndicator(
+      alternativeWidget: alternativeWidget,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final channel = StreamChannel.of(context).channel;
     return Row(
       children: [
-        Avatar.small(url: messageData.profilePic),
+        Avatar.small(
+            url: Helpers.getChannelImage(channel, context.currentUser!)),
         SizedBox(
           width: 16,
         ),
@@ -77,19 +202,43 @@ class _AppBarTitle extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                messageData.senderName,
+                Helpers.getChannelName(channel, context.currentUser!),
                 overflow: TextOverflow.ellipsis,
                 style: const TextStyle(fontSize: 14),
               ),
               SizedBox(
                 height: 12,
               ),
-              Text(
-                'Online now',
-                style: TextStyle(
-                    fontSize: 10,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green),
+              BetterStreamBuilder<List<Member>>(
+                stream: channel.state!.membersStream,
+                initialData: channel.state!.members,
+                builder: (context, data) {
+                  return ConnectionStatusBuilder(
+                      statusBuilder: (context, status) {
+                    switch (status) {
+                      case ConnectionStatus.connected:
+                        return _buildConnectedTitleState(context, data);
+                      case ConnectionStatus.connecting:
+                        return Text(
+                          'Connecting',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green),
+                        );
+                      case ConnectionStatus.disconnected:
+                        return Text(
+                          'Offline',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green),
+                        );
+                      default:
+                        return SizedBox.shrink();
+                    }
+                  });
+                },
               ),
             ],
           ),
@@ -99,33 +248,123 @@ class _AppBarTitle extends StatelessWidget {
   }
 }
 
-class _DemoMessageList extends StatelessWidget {
-  const _DemoMessageList({Key? key}) : super(key: key);
+class TypingIndicator extends StatelessWidget {
+  final Widget? alternativeWidget;
+  const TypingIndicator({
+    Key? key,
+    this.alternativeWidget,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: ListView(
-        children: [
-          _DateLable(lable: 'Yesterday'),
-          _MessageTile(
-            message: 'Hi',
-            messageDate: '11:01 PM',
+    final channelState = StreamChannel.of(context).channel.state!;
+
+    final altWidget = alternativeWidget ?? const SizedBox.shrink();
+
+    return BetterStreamBuilder<Iterable<User>>(
+      initialData: channelState.typingEvents.keys,
+      stream: channelState.typingEventsStream
+          .map((typings) => typings.entries.map((e) => e.key)),
+      builder: (context, data) {
+        return Align(
+          alignment: Alignment.centerLeft,
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            child: data.isNotEmpty == true
+                ? const Align(
+                    alignment: Alignment.centerLeft,
+                    key: ValueKey('typing-text'),
+                    child: Text(
+                      'Typing message',
+                      maxLines: 1,
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                : Align(
+                    alignment: Alignment.centerLeft,
+                    key: const ValueKey('altwidget'),
+                    child: altWidget,
+                  ),
           ),
-          _MessageOwnTile(
-            message: 'Hello',
-            messageDate: '11:01 PM',
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
 
-class _DateLable extends StatelessWidget {
-  final String lable;
-  const _DateLable({Key? key, required this.lable}) : super(key: key);
+class ConnectionStatusBuilder extends StatelessWidget {
+  final Stream<ConnectionStatus>? connectionStatusStream;
+  final Widget Function(BuildContext context, Object? error)? errorBuilder;
+  final WidgetBuilder? loadingBuilder;
+  final Widget Function(BuildContext context, ConnectionStatus status)
+      statusBuilder;
+  const ConnectionStatusBuilder(
+      {Key? key,
+      required this.statusBuilder,
+      this.connectionStatusStream,
+      this.errorBuilder,
+      this.loadingBuilder})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final stream = connectionStatusStream ??
+        StreamChatCore.of(context).client.wsConnectionStatusStream;
+    final client = StreamChatCore.of(context).client;
+
+    return BetterStreamBuilder<ConnectionStatus>(
+      initialData: client.wsConnectionStatus,
+      stream: stream,
+      noDataBuilder: loadingBuilder,
+      errorBuilder: (context, error) {
+        if (errorBuilder != null) {
+          return errorBuilder!(context, error);
+        }
+        return Offstage();
+      },
+      builder: statusBuilder,
+    );
+  }
+}
+
+class _DateLable extends StatefulWidget {
+  final DateTime dateTime;
+  const _DateLable({Key? key, required this.dateTime}) : super(key: key);
+
+  @override
+  State<_DateLable> createState() => _DateLableState();
+}
+
+class _DateLableState extends State<_DateLable> {
+  late String dayInfo;
+
+  @override
+  void initState() {
+    final createdAt = Jiffy.parse(widget.dateTime.toString());
+    final now = DateTime.now();
+
+    if (Jiffy.parse(createdAt.toString()).isSame(Jiffy.parse(now.toString()))) {
+      dayInfo = 'TODAY';
+    } else if (Jiffy.parse(createdAt.toString()).isSame(
+        Jiffy.parse(now.subtract(const Duration(days: 1)).toString()))) {
+      dayInfo = 'YESTERDAY';
+    } else if (Jiffy.parse(createdAt.toString()).isAfter(
+      Jiffy.parse(now.subtract(const Duration(days: 7)).toString()),
+    )) {
+      dayInfo = createdAt.EEEE;
+    } else if (Jiffy.parse(createdAt.toString()).isAfter(
+      Jiffy.parse(now.toString()).subtract(years: 1),
+    )) {
+      dayInfo = createdAt.MMMd;
+    } else {
+      dayInfo = createdAt.MMMd;
+    }
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -140,7 +379,7 @@ class _DateLable extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
             child: Text(
-              lable,
+              dayInfo,
               style: TextStyle(
                 fontSize: 12,
                 fontWeight: FontWeight.bold,
@@ -155,14 +394,11 @@ class _DateLable extends StatelessWidget {
 }
 
 class _MessageTile extends StatelessWidget {
-  final String message;
-  final String messageDate;
+  final Message message;
 
   static const _borderRadius = 26.0;
 
-  const _MessageTile(
-      {Key? key, required this.message, required this.messageDate})
-      : super(key: key);
+  const _MessageTile({Key? key, required this.message}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -185,13 +421,13 @@ class _MessageTile extends StatelessWidget {
               ),
               child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
-                child: Text(message),
+                child: Text(message.text ?? ''),
               ),
             ),
             Padding(
               padding: EdgeInsets.only(top: 8.0),
               child: Text(
-                messageDate,
+                Jiffy.parse(message.createdAt.toLocal().toString()).jm,
                 style: TextStyle(
                   color: AppColors.textFaded,
                   fontSize: 12,
@@ -207,14 +443,11 @@ class _MessageTile extends StatelessWidget {
 }
 
 class _MessageOwnTile extends StatelessWidget {
-  final String message;
-  final String messageDate;
+  final Message message;
 
   static const _borderRadius = 26.0;
 
-  const _MessageOwnTile(
-      {Key? key, required this.message, required this.messageDate})
-      : super(key: key);
+  const _MessageOwnTile({Key? key, required this.message}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -237,13 +470,13 @@ class _MessageOwnTile extends StatelessWidget {
               ),
               child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 20),
-                child: Text(message),
+                child: Text(message.text ?? ''),
               ),
             ),
             Padding(
               padding: EdgeInsets.only(top: 8.0),
               child: Text(
-                messageDate,
+                Jiffy.parse(message.createdAt.toLocal().toString()).jm,
                 style: TextStyle(
                   color: AppColors.textFaded,
                   fontSize: 12,
@@ -258,8 +491,49 @@ class _MessageOwnTile extends StatelessWidget {
   }
 }
 
-class _ActionBar extends StatelessWidget {
+class _ActionBar extends StatefulWidget {
   const _ActionBar({Key? key}) : super(key: key);
+
+  @override
+  __ActionBarState createState() => __ActionBarState();
+}
+
+class __ActionBarState extends State<_ActionBar> {
+  final StreamMessageInputController controller =
+  StreamMessageInputController();
+
+  Timer? _debounce;
+
+  Future<void> _sendMessage() async {
+    if (controller.text.isNotEmpty) {
+      StreamChannel.of(context).channel.sendMessage(controller.message);
+      controller.clear();
+      FocusScope.of(context).unfocus();
+    }
+  }
+
+  void _onTextChange() {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(seconds: 1), () {
+      if (mounted) {
+        StreamChannel.of(context).channel.keyStroke();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_onTextChange);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_onTextChange);
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -270,35 +544,47 @@ class _ActionBar extends StatelessWidget {
         children: [
           Container(
             decoration: BoxDecoration(
-                border: Border(
-                    right: BorderSide(
-              width: 2,
-              color: Theme.of(context).dividerColor,
-            ))),
-            child: Padding(
+              border: Border(
+                right: BorderSide(
+                  width: 2,
+                  color: Theme.of(context).dividerColor,
+                ),
+              ),
+            ),
+            child: const Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Icon(Icons.camera_alt),
+              child: Icon(
+                Icons.camera_alt,
+              ),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: EdgeInsets.only(left: 16.0),
+              padding: const EdgeInsets.only(left: 16.0),
               child: TextField(
-                style: TextStyle(fontSize: 14),
-                decoration: InputDecoration(
-                  hintText: 'Type something',
+                controller: controller.textFieldController,
+                onChanged: (val) {
+                  controller.text = val;
+                },
+                style: const TextStyle(fontSize: 14),
+                decoration: const InputDecoration(
+                  hintText: 'Type something...',
                   border: InputBorder.none,
                 ),
+                onSubmitted: (_) => _sendMessage(),
               ),
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(
+            padding: const EdgeInsets.only(
               left: 12,
               right: 24.0,
             ),
             child: GlowingActionButton(
-                color: AppColors.accent, icon: Icons.send, onPressed: () {}),
+              color: AppColors.accent,
+              icon: Icons.send_rounded,
+              onPressed: _sendMessage,
+            ),
           ),
         ],
       ),
